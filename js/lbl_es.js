6007b4819c705cc0754b92d67332995330f018be
villatore <villatore@yahoo.com.mx>
2013-10-28 09:18:39 -0600

    1

--- js/lbl_es.js
+++ js/lbl_es.js
@@ -2,15 +2,15 @@ var l8n = {
     "en-US": {
         "#version": "Version 0.0.1en",
         "#ajustes": "Settings",
-        "#per": "Rol"
+        "#per": "Rol",
         "#sal" : "Exit"
     },
     "es": { //español
     },
     "es-419": { //español - latinoamerica
         "#version": "Version 0.0.1es",
-        "#ajustes": "Ajusteses"
-        "#per": "Perfil"
+        "#ajustes": "Ajustes",
+        "#per": "Perfil",
         "#sal" : "Salida"
     },
     "ko": {
@@ -23,6 +23,8 @@ function Textos() {
     l8n["es"] = l8n["es-419"];
     Etiqueta("#version");
     Etiqueta("#ajustes");
+    Etiqueta("#per");
+    Etiqueta("#sal");
 }
 function Etiqueta(key) {
     var lenguaje = "en-US"; //idioma por default
