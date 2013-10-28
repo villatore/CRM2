094432ef11d95af449d00bbd7c4a33a3d04b3ce3
villatore <villatore@yahoo.com.mx>
2013-10-28 07:33:15 -0600

    11

--- js/index.js
+++ js/index.js
@@ -406,18 +406,3 @@
         $('.detailPayments').listview('refresh');
     });
 })(jQuery);
-
-var app = {
-    // Application Constructor
-    initialize: function () {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicity call 'app.receivedEvent(...);'
-    onDeviceReady: function () {
-        alert("cambiara la pagina");
-        window.location = "index2.html";
-    },
-};
